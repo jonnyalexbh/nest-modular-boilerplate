859095e218fff9cc4c0e25c4bf91e9a842f58777
jonnyalexbh <jonnyalex.bh@gmail.com>
2022-02-02 10:10:13 -0500

    Implementing Mongoose in Modules

--- src/clients/clients.module.ts
+++ src/clients/clients.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ClientsController } from './controllers/clients.controller';
 import { ClientsService } from './services/clients.service';
+import { Client, ClientSchema } from './entities/clients.entity';
 
 @Module({
-  imports: [],
+  imports: [
+    MongooseModule.forFeature([{ name: Client.name, schema: ClientSchema }]),
+  ],
   controllers: [ClientsController],
   providers: [ClientsService],
   exports: [],
--- src/clients/entities/clients.entity.ts
+++ src/clients/entities/clients.entity.ts
@@ -1,6 +1,18 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Document } from 'mongoose';
+
+export type ClientDocument = Client & Document;
+
+@Schema()
 export class Client {
-  id: number;
+  @Prop({ required: true })
   name: string;
+
+  @Prop()
   age: number;
+
+  @Prop()
   email: string;
 }
+
+export const ClientSchema = SchemaFactory.createForClass(Client);
